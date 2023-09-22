34a9f6eb6fcec75ba111f8017b90304400a7d3b9
vladimir <tarskij-vladimir@yandex.ru>
2023-09-22 16:52:54 +0300

    added task3

---
+++ 3_task/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.WriteLine("Введите число :   ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+int ostatok = N%2; 
+if (ostatok ==0)
+	Console.WriteLine("число четное");
+else
+	Console.WriteLine("число нечетное");

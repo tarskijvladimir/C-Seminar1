ff2799aed0638206f2882ee27aa69892d16e1b63
vladimir <tarskij-vladimir@yandex.ru>
2023-09-22 19:40:10 +0300

    added task4

---
+++ 4_task/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Write("Введите число :   ");
+int N = Convert.ToInt32(Console.ReadLine());
+int num = N%2 +1;
+if(N>0)   
+{
+    if (num ==2)
+    {
+     while(num<=N){
+        System.Console.Write($"{num} ");
+        num = num + 2;    }
+    }      
+    else
+    {
+        num = num + 1;
+        while(num<=N){
+            System.Console.Write($"{num} ");
+            num = num + 2;}
+    }
+}
+  else
+{
+    if (num == 0)
+    {
+     while(num>=N){
+        System.Console.Write($"{num} ");
+        num = num - 2;    }
+    }      
+    else
+    {
+        num = num - 1;
+        while(num>=N){
+            System.Console.Write($"{num} ");
+            num = num - 2;}
+    }
+}
+
+ 

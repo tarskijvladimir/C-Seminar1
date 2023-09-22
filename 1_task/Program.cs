7251444073497ee3973c5187b53b232afc1c465b
vladimir <tarskij-vladimir@yandex.ru>
2023-09-22 13:31:45 +0300

    added changes2

--- 1_task/Program.cs
+++ 1_task/Program.cs
@@ -1,6 +1,9 @@
 ﻿
-int a = - 2;
-int b = - 5;
+Console.Write("Введите число a:   "); //ввод данных
+int a = Convert.ToInt32(Console.ReadLine());// преобразование в тип, т.к Console это строка – мы принимаем сточку – и нам надо преобразовать в тип int
+Console.Write("Введите число b:   ");
+int b = Convert.ToInt32(Console.ReadLine());
+
 if(a > b)
 {
     Console.WriteLine("max = a ");
@@ -9,3 +12,4 @@ else
 {
     Console.WriteLine("max = b");
 }
+
